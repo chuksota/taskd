c5275880bb9708db5c3c40bdb7d2b25cdf5eab8e
Josh Steinberg <jpsteinberg@gmail.com>
2021-04-05 13:37:39 -0400

    user model created

--- config/index.js
+++ config/index.js
@@ -1,5 +1,7 @@
+const dotenv = require("dotenv");
+dotenv.config();
 module.exports = {
-  environment: process.env.NODE_ENV || 'development',
+  environment: process.env.NODE_ENV || "development",
   port: process.env.PORT || 8080,
   db: {
     username: process.env.DB_USERNAME,
---
+++ db/models/user.js
@@ -0,0 +1,16 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      userName: { type: DataTypes.STRING(100), allowNull: false, unique: true },
+      email: { type: DataTypes.STRING(100), allowNull: false, unique: true },
+      hashedPassword: { type: DataTypes.STRING.BINARY, allowNull: false },
+    },
+    {}
+  );
+  User.associate = function (models) {
+    // associations can be defined here
+  };
+  return User;
+};
