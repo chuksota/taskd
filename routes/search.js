3d72db3fdfef3a7b29899c6beacb429a431eb850
Josh Steinberg <jpsteinberg@gmail.com>
2021-04-08 13:02:45 -0400

    search route task/list complete

--- routes/search.js
+++ routes/search.js
@@ -3,31 +3,48 @@ const router = express.Router();
 const db = require("../db/models");
 const { asyncHandler, csrfProtection } = require("./utils");
 const { check, validationResult } = require("express-validator");
-const {Op} = require('sequelize');
+const { Op } = require("sequelize");
 
-const {List, Task} = db;
+const { List, Task } = db;
 
-router.get('/lists/:input', asyncHandler(async (req, res)=>{
-    // const {userId} = req.session.auth;
-    const userId = 1 //remove later
+router.get(
+  "/lists/:input",
+  asyncHandler(async (req, res) => {
+    const { userId } = req.session.auth;
     const input = req.params.input;
     console.log(input);
-    const foundLists = await List.findAll({ where: { userId, name: {[Op.iLike]: `%${input}%`}}})
+    const foundLists = await List.findAll({
+      where: { userId, name: { [Op.iLike]: `%${input}%` } },
+    });
 
-    res.json({foundLists});
-}))
+    res.json({ foundLists });
+  })
+);
 
-router.get('/tasks/:input', asyncHandler(async (req, res)=> {
-    // const {userId} = req.session.auth;
-    const userId = 1 //remove later
+router.get(
+  "/tasks/:input",
+  asyncHandler(async (req, res) => {
+    const { userId } = req.session.auth;
     const input = req.params.input;
     console.log(input);
 
-    const foundListsWithTasks = await List.findAll({where: {userId}, include: [{model: Task, where: {description: {[Op.iLike]: `%${input}%`}}}]})
+    const foundListsWithTasks = await List.findAll({
+      where: { userId },
+      include: [
+        { model: Task, where: { description: { [Op.iLike]: `%${input}%` } } },
+      ],
+    });
 
-    
-    
-    res.json({foundTasks});
-}))
+    const foundTasks = [];
+
+    foundListsWithTasks.forEach((list) => {
+      list.Tasks.forEach((task) => {
+        foundTasks.push(task);
+      });
+    });
+
+    res.json({ foundTasks });
+  })
+);
 
 module.exports = router;
