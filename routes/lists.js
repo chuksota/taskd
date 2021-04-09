bb45109870ecaf053f8eb57417a89e1c0ef0afeb
Josh Steinberg <jpsteinberg@gmail.com>
2021-04-09 12:37:03 -0400

    saved demo data from being deleted

--- routes/lists.js
+++ routes/lists.js
@@ -5,61 +5,82 @@ const { asyncHandler, csrfProtection } = require("./utils");
 const { check, validationResult } = require("express-validator");
 
 const listsValidators = [
-  check('name')
-    .exists({checkFalsy: true})
-    .withMessage('Please provide a list name')
-    .isLength({max: 50})
-    .withMessage('List name must not be more than 50 characters')
-]
+  check("name")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a list name")
+    .isLength({ max: 50 })
+    .withMessage("List name must not be more than 50 characters"),
+];
 
-router.get('/', asyncHandler(async(req, res)=>{
-  const {userId} = req.session.auth
-  const lists = await db.List.findAll({where: {userId}})
-  res.json({lists})
-}))
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { userId } = req.session.auth;
+    const lists = await db.List.findAll({ where: { userId } });
+    res.json({ lists });
+  })
+);
 
-router.post('/', listsValidators,  asyncHandler(async(req, res)=>{
-  const {name, dueDate} = req.body
-  const {userId} = req.session.auth
-  const newList =  db.List.build({
-    name,
-    dueDate,
-    userId
-  });
-const validatorErrors = validationResult(req)
-if(validatorErrors.isEmpty()){
-  await newList.save()
-  res.json({newList})
-} else {
-  const errors = validatorErrors.array().map((error)=> error.msg)
-  res.json({errors})
-}
-}));
+router.post(
+  "/",
+  listsValidators,
+  asyncHandler(async (req, res) => {
+    const { name, dueDate } = req.body;
+    const { userId } = req.session.auth;
+    const newList = db.List.build({
+      name,
+      dueDate,
+      userId,
+    });
+    const validatorErrors = validationResult(req);
+    if (validatorErrors.isEmpty()) {
+      if (userId !== 1) {
+        await newList.save();
+      }
+      res.json({ newList });
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.json({ errors });
+    }
+  })
+);
 
+router.put(
+  "/:id(\\d+)",
+  listsValidators,
+  asyncHandler(async (req, res, next) => {
+    const { userId } = req.session.auth;
+    const listId = parseInt(req.params.id);
+    const list = await db.List.findByPk(listId);
+    const { name, dueDate, completed } = req.body;
+    if (list) {
+      if (userId !== 1) {
+        await list.update({ name, dueDate, completed });
+      }
+      res.json({ list });
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.json({ errors });
+    }
+  })
+);
 
-router.put('/:id(\\d+)', listsValidators, asyncHandler(async(req, res, next)=> {
-  const listId = parseInt(req.params.id)
-  const list = await db.List.findByPk(listId)
-  const {name, dueDate, completed} = req.body
-  if(list){
-    await list.update({name, dueDate, completed})
-    res.json({list})
-  } else {
-    const errors = validatorErrors.array().map((error)=> error.msg)
-    res.json({errors})
-  }
-}));
+router.delete(
+  "/:id(\\d+)",
+  asyncHandler(async (req, res, next) => {
+    const listId = parseInt(req.params.id);
+    const list = await db.List.findByPk(listId);
+    const { userId } = req.session.auth;
+    if (list) {
+      if (userId !== 1) {
+        await list.destroy();
+      }
+      res.status(204).end();
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.json({ errors });
+    }
+  })
+);
 
-router.delete('/:id(\\d+)', asyncHandler(async(req, res, next)=>{
-  const listId = parseInt(req.params.id)
-  const list = await db.List.findByPk(listId)
-  if(list){
-    await list.destroy()
-    res.status(204).end()
-  } else {
-    const errors = validatorErrors.array().map((error)=> error.msg)
-    res.json({errors})
-  }
-}))
-
-module.exports = router
+module.exports = router;
--- routes/tasks.js
+++ routes/tasks.js
@@ -3,74 +3,95 @@ const router = express.Router();
 const db = require("../db/models");
 const { asyncHandler, csrfProtection } = require("./utils");
 const { check, validationResult } = require("express-validator");
-const {Task} = db;
+const { Task } = db;
 
-router.get('/:listId(\\d+)', asyncHandler(async (req, res)=> {
+router.get(
+  "/:listId(\\d+)",
+  asyncHandler(async (req, res) => {
     const listId = parseInt(req.params.listId);
-    const tasks = await db.Task.findAll({where: {listId}})
+    const tasks = await db.Task.findAll({ where: { listId } });
 
-    res.json({tasks})
-}))
+    res.json({ tasks });
+  })
+);
 
 const taskValidators = [
-    check('description')
-        .exists({checkFalsy: true})
-        .withMessage('Please enter a description')
-        .isLength({max: 200})
-        .withMessage('descirption cannot be more than 200 characters'),
-    check('notes').isLength({max: 500}).withMessage('notes cannot be more than 500 characters'),
-]
+  check("description")
+    .exists({ checkFalsy: true })
+    .withMessage("Please enter a description")
+    .isLength({ max: 200 })
+    .withMessage("descirption cannot be more than 200 characters"),
+  check("notes")
+    .isLength({ max: 500 })
+    .withMessage("notes cannot be more than 500 characters"),
+];
 
-router.post('/', taskValidators, asyncHandler(async (req, res)=>{ 
-    const {description, notes, dueDate, listId} = req.body;
+router.post(
+  "/",
+  taskValidators,
+  asyncHandler(async (req, res) => {
+    const { description, notes, dueDate, listId } = req.body;
+    const { userId } = req.session.auth;
+    const newTask = await Task.build({ description, notes, dueDate, listId });
 
-    const newTask = await Task.build({description, notes, dueDate, listId});
-
-    const validationErrors = validationResult(req)
-    if(validationErrors.isEmpty()){
-       await newTask.save()
-       res.json({newTask});
+    const validationErrors = validationResult(req);
+    if (validationErrors.isEmpty()) {
+      if (userId !== 1) {
+        await newTask.save();
+      }
+      res.json({ newTask });
     } else {
-        const errors = validatorErrors.array().map((error) => error.msg);
-        res.json({errors})
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.json({ errors });
     }
+  })
+);
 
-}))
-
-router.put('/:id(\\d+)', taskValidators, asyncHandler(async (req, res) =>{
+router.put(
+  "/:id(\\d+)",
+  taskValidators,
+  asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id);
     const task = await Task.findByPk(id);
+    const { userId } = req.session.auth;
+    const { description, notes, dueDate, completed } = req.body;
 
-    const {description, notes, dueDate, completed} = req.body;
-
-    const validationErrors = validationResult(req)
+    const validationErrors = validationResult(req);
     if (!validationErrors.isEmpty()) {
-        const errors = validatorErrors.array().map((error) => error.msg);
-        res.json({ errors })
-        return
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.json({ errors });
+      return;
     }
 
-    if(task){
-        task.description = description;
-        task.notes = notes;
-        task.dueDate = dueDate;
-        task.completed = completed;
+    if (task) {
+      task.description = description;
+      task.notes = notes;
+      task.dueDate = dueDate;
+      task.completed = completed;
+      if (userId !== 1) {
         await task.save();
-        res.json({task})
+      }
+      res.json({ task });
+    } else {
+      const error = new Error(`Task ${id} not found!`);
+      error.status = 404;
+      error.title = "Task not found";
+      res.json({ error });
     }
-    else {
-        const error = new Error(`Task ${id} not found!`)
-        error.status = 404
-        error.title = 'Task not found'
-        res.json({error})
-    }
-}))
+  })
+);
 
-router.delete('/:id(\\d+)', asyncHandler(async (req, res) => {
+router.delete(
+  "/:id(\\d+)",
+  asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id);
+    const { userId } = req.session.auth;
     const task = await Task.findByPk(id);
-    await task.destroy();
+    if (userId !== 1) {
+      await task.destroy();
+    }
     res.status(204).end();
-}))
+  })
+);
 
-module.exports = router
+module.exports = router;
