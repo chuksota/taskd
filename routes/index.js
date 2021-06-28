b74fe8fdffad4b289004b13fe920384771c2e4a6
Elias Eichen <eliprof@live.com>
2021-06-28 14:35:14 -0500

    fixed splash showing up when logged in

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+// const { requireAuth } = require("../auth");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'taskd' });
+  if(res.locals.authenticated){
+    return res.render('homepage')
+  }else{
+
+    res.render('index', { title: 'taskd' });
+  }
 });
 
 module.exports = router;
